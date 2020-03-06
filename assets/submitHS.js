0385361997c83d81386a2e3c6cb725ca84e4c094
Chyna Davis <chyna.davis11@gmail.com>
2020-03-06 02:05:10 -0700

    make both HighscorePages functional

--- assets/submitHS.js
+++ assets/submitHS.js
@@ -1,9 +1,24 @@
-// get user's final score from LOCAL STORAGE and display it on submit-highscores-page.html
+/////////////////////////////////// VARIABLES ///////////////////////////////////
 var finalScore = document.getElementById("finalScore");
-var userScore = localStorage.getItem("userScore");
+var usernameInput = document.getElementById("username");
+var saveScoreBtn = document.getElementById("saveScoreBtn");
 
-finalScore.innerText = userScore;
+var highScores = JSON.parse(localStorage.getItem("highscores")) || [];
+console.log(highScores);
 
-///////////////////////////////////////////////////////////////////////////////
+var userScore = localStorage.getItem("userScore"); // user score from local storage ...
+finalScore.innerText = userScore; // displayed on submit-highscores-page.html
 
 /////////////////////////////////// FUNCTIONS ///////////////////////////////////
+// function to make SaveScore Button work; when the user types their name into the input field and clicks "save", their name and score will be added to localStorage as values in an array, and the page will be redirected to the View Highscores page, where they will see their score listed
+saveScoreBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    var userAndScore = [usernameInput.value, userScore];
+
+    highScores.push(userAndScore);
+
+    localStorage.setItem("highscores", JSON.stringify(highScores));
+
+    return window.location.assign("view-highscores-page.html");
+});
--- assets/viewHS.js
+++ assets/viewHS.js
@@ -1,1 +1,43 @@
-// note: display username and score is <li> elements with the class "highscore-item" (for CSS purposes)
+/////////////////////////////////// VARIABLES ///////////////////////////////////
+var highscoreList = document.getElementById("highscoreList"); // ul
+var clearAllBtn = document.getElementById("clearAll-btn");
+
+// ===============================================================================
+
+var userScores = [];
+
+openPage();
+
+function renderHighscores() {
+    highscoreList.innerHTML = "";
+
+    for (var i = 0; i < userScores.length; i++) {
+        var userScore = userScores[i];
+    
+        var li = document.createElement("li");
+        li.textContent = userScore[0] + " ----- " + userScore[1] + " points";
+        li.setAttribute("data-index", i);
+        li.classList.add("highscore-item");
+    
+        highscoreList.appendChild(li);
+    }
+}
+
+function openPage() {
+    var highScores = JSON.parse(localStorage.getItem("highscores"));
+  
+    if (highScores !== null) {
+        userScores = highScores;
+    }
+  
+    renderHighscores();
+  }
+
+// ===============================================================================
+
+// clearAllBtn function: adds event listener for the button, clears out the localStorage, and changes the inner HTML by showing the empty storage
+clearAllBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+    localStorage.clear();
+    highscoreList.innerHTML = [];
+});
